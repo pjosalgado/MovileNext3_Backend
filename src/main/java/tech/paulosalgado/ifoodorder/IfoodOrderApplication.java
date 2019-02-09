6b28f1319329422426efa9dee4dd17539ab34aee
Paulo Salgado <pjosalgado@gmail.com>
2019-02-09 13:41:10 -0200

    Projeto inicial.

---
+++ src/main/java/tech/paulosalgado/ifoodorder/IfoodOrderApplication.java
@@ -0,0 +1,13 @@
+package tech.paulosalgado.ifoodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfoodOrderApplication.class, args);
+    }
+
+}
